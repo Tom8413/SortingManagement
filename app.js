3bd0baefd9200e98bb9d38bbc67f64d77586d739
Tomek <sigma345@wp.pl>
2025-09-11 21:21:15 +0200

    Run server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("start");
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    res.send('<p>Write something</p>')
+});
