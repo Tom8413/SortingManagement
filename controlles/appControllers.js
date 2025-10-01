39af6e2ed56d7aaa5dbc5501de9e278b76ccc092
Tomek <sigma345@wp.pl>
2025-10-01 21:19:45 +0200

    add appController

---
+++ controlles/appControllers.js
@@ -0,0 +1,21 @@
+const Employee = require('../modules/ShameTemplate');
+
+const employee_details = (req, res) => {
+    const employee = new Employee({
+        ID: 1003,
+        nameAndSurname: 'Adam Lolek',
+        available: true
+    });
+
+    employee.save()
+        .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+};
+
+module.exports = {
+    employee_details
+};
