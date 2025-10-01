56a929f6e839c052dec61720c221e6019d520b83
Tomek <sigma345@wp.pl>
2025-10-01 20:51:14 +0200

    use appRoutes in app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
-const Employee = require('./modules/ShameTemplate');
+
+const appRouter = require('./routes/appRoutes');
 
 
 const app = express();
@@ -15,18 +15,4 @@ mongoose.connect(dbURI)
 .then((result) => app.listen(3000))
 .catch((err) => console.log(err));
 
-app.get('/add-employee', (req, res) => {
-    const employee = new Employee({
-        ID: 1000,
-        nameAndSurname: 'Adam Kwiatkowski',
-        available: true
-    });
-
-    employee.save()
-        .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    }) 
-})
+app.use(appRouter);
