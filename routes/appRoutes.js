7447075d3c1922c52a15eac028a1544a44161ddd
Tomek <sigma345@wp.pl>
2025-10-01 21:20:55 +0200

    move shema logic to app controller

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,21 +1,7 @@
 const express = require('express');
-const Employee = require('../modules/ShameTemplate');
+const appController = require('../controlles/appControllers');
 const router = express.Router();
 
-router.get('/add-employee', (req, res) => {
-    const employee = new Employee({
-        ID: 1002,
-        nameAndSurname: 'Adam Lolek',
-        available: true
-    });
-
-    employee.save()
-        .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    }) 
-})
+router.get('/add-employee', appController.employee_details);
 
 module.exports = router;
