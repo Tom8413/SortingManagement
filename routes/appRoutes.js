6b8552f2862ecc189587f151ca9f558bd2c8e92d
Tomek <sigma345@wp.pl>
2025-10-09 22:33:44 +0200

    fix router

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const appController = require('../controlles/appControllers');
 const router = express.Router();
 
-router.get('/add-employee', appController.employee_details);
+//router.get('/add-employee', appController.employee_details);
 
+router.get('/create-employee', appController.create_employee);
 router.post('/create-employee', appController.create_employee);
 
 module.exports = router;
