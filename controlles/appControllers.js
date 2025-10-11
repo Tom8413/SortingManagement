2268a585bf495954c1eb2e56fd396b4d4d256129
Tomek <sigma345@wp.pl>
2025-10-11 21:43:22 +0200

    add employee_details function

--- controlles/appControllers.js
+++ controlles/appControllers.js
@@ -1,19 +1,33 @@
 const Employees = require('../modules/ShameTemplate');
 
+
+
+const employee_details = (req, res) => {
+    Employees.find()
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((error) => {
+            res.status(500).json(error)
+        })
+    };
+
+    
 const create_employee = (req, res) => {
     const employee = new Employees(req.body);
 
     employee.save()
         .then((result) => {
-        res.status(200).send(result)
-        console.log(req.body)
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+            res.status(200).send(result)
+            console.log(req.body)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 };
 
 module.exports = {
 
-    create_employee
+    create_employee,
+    employee_details,
 };
