fdad32eae10f840ef152dcf380957ced09349fd3
Tomek <sigma345@wp.pl>
2025-10-11 21:43:55 +0200

    add router.get function

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const appController = require('../controlles/appControllers');
 const router = express.Router();
 
-//router.get('/add-employee', appController.employee_details);
+router.get('/show-employee', appController.employee_details);
 
 router.post('/create-employee', appController.create_employee);
 
