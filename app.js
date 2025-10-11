77ed63795b5d43ba801cddb97c3533d55b728540
Tomek <sigma345@wp.pl>
2025-10-11 12:49:57 +0200

    add bodyParser

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors')
 const appRouter = require('./routes/appRoutes');
+const bodyParser = require('body-parser');
 
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors({
     origin: ['http://localhost:8080'],
@@ -21,4 +25,5 @@ mongoose.connect(dbURI)
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
 
+app.use(express.urlencoded({extended:true}));
 app.use(appRouter);
