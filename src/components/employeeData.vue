ec79a1fa4dfd400d674ba725f5fc18dd05428aff
Tomek <sigma345@wp.pl>
2025-10-16 17:24:48 +0200

    add axios.get and frontend

---
+++ src/components/employeeData.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="container">
+    <AddEmployeeData></AddEmployeeData>
+    <div class="col-lg-3">
+        <div class="panel panel-primary">
+            <div class="panel-heading">MUB
+                <span class="badge pull-right">1</span>
+            </div>
+            <div class="panel-body">
+                <kbd>2</kbd> People
+                <kbd>1</kbd> Kids
+                <br>
+                <br>
+                <kbd>Suit</kbd>
+                <button type="button" class="btn btn-dagner pull-right">Delete</button>
+            </div>
+            <div class="panel-footer">
+                <h4>
+                    Cosadasjdjasd
+                </h4>
+            </div>
+        </div>
+    </div>
+    </div>  
+</template>
+
+<script>
+import axios from 'axios';
+import AddEmployeeData from './AddEmployeeData.vue';
+
+export default {
+    name: 'employeeData',
+    
+    data() {
+        return {
+            msg: "Good",
+            reciveData: []
+        }
+    },
+    created() {
+        axios.get('http://localhost:3000/show-employee')
+        .then((response) => {
+            this.reciveData = response.data;
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+}
+</script>
+
+<style>
+
+</style>
