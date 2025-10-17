44c8863c8e17d7d618c29583029649724b9975bb
Tomek <sigma345@wp.pl>
2025-10-17 20:35:03 +0200

    add router.delete()

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -6,5 +6,7 @@ router.get('/show-employee', appController.employee_details);
 
 router.post('/create-employee', appController.create_employee);
 
+router.delete('/delete-employee/:id', appController.delete_employee);
+
 
 module.exports = router;
