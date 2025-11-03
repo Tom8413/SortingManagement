219573532735907b7a25f6e93a933eb2591a8fe0
Tomek <sigma345@wp.pl>
2025-11-03 21:36:35 +0100

    Add closeForm, cancelForm function

--- src/components/AddEmployeeData.vue
+++ src/components/AddEmployeeData.vue
@@ -1,90 +1,102 @@
 <template>
+  <div class="backdrop" @click.self="closeForm">
+    <form>
+      <label>First Name:</label>
+      <input type="First Name" required v-model="Empolyee.first_name" />
 
-<form>
-    <label>First Name:</label>
-    <input type="First Name" required v-model="Empolyee.first_name">
+      <label>Last Name:</label>
+      <input type="text" required v-model="Empolyee.last_name" />
 
-    <label>Last Name:</label>
-    <input type="text" required v-model="Empolyee.last_name">
+      <label>ID_number:</label>
+      <input type="text" required v-model="Empolyee.ID_number" />
 
-    <label>ID_number:</label>
-    <input type="text" required v-model="Empolyee.ID_number">
+      <label>Department:</label>
+      <input type="text" required v-model="Empolyee.Department" />
 
-    <label>Department:</label>
-    <input type="text" required v-model="Empolyee.Department">
-
-    <button type="button" @click="addToAPI">Submit</button>
-</form>
-
-
+      <button type="button" @click="addToAPI">Submit</button>
+      <button type="button" @click="closeForm">Cancel</button>
+    </form>
+  </div>
 </template>
 
 
 
 <script>
-import axios from 'axios';
-
+import axios from "axios";
 
 export default {
-    name: 'addEmployeeData',
-    data() {
-        return {
-            Empolyee: { first_name: '', last_name: '', ID_number: '', Department: ''},
-           
-        }
-    },
-    methods: {
+  name: "addEmployeeData",
+  data() {
+    return {
+      Empolyee: {
+        first_name: "",
+        last_name: "",
+        ID_number: "",
+        Department: "",
+      },
+    };
+  },
+  methods: {
     addToAPI() {
-        let newUser = {
-            first_name: this.Empolyee.first_name,
-            last_name: this.Empolyee.last_name,
-            ID_number: this.Empolyee.ID_number,
-            Department: this.Empolyee.Department
-        }
-        console.log(newUser);
-        axios.post('http://localhost:3000/create-employee', newUser)
+      let newUser = {
+        first_name: this.Empolyee.first_name,
+        last_name: this.Empolyee.last_name,
+        ID_number: this.Empolyee.ID_number,
+        Department: this.Empolyee.Department,
+      };
+      console.log(newUser);
+      axios
+        .post("http://localhost:3000/create-employee", newUser)
         .then((response) => {
-            console.log(response);
+          console.log(response);
         })
         .catch((error) => {
-            console.log(error);
+          console.log(error);
         });
-    }
-}
-
-}
+    },
+    closeForm() {
+      this.$emit("closeFormEmit");
+    },
+  },
+};
 </script>
 
 <style>
 form {
-    max-width: 420px;
-    margin: 30px auto;
-    background: white;
-    text-align: left;
-    padding: 40px;
-    border-radius:  10px;
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
 }
 label {
-    color: #aaa;
-    display: inline-block;
-    margin: 25px 0 15px;
-    font-size: 0.9em;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: bold;
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.9em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
 }
 input {
-    display: block;
-    padding: 10px 6px;
-    width: 100%;
-    box-sizing: border-box;
-    border: none;
-    border-bottom: 1px solid #ddd;
-    color: #555;
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
 }
 button {
-    width: 100%;
-    margin: 30px;
+  width: 100%;
+  margin: 30px 0px 5px;
 }
-
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
 </style>
