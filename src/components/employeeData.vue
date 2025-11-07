a7d934b68fb91cbcec9971698d9e148456e58e57
Tomek <sigma345@wp.pl>
2025-11-07 20:04:10 +0100

    add update() to emit data to parent

--- src/components/employeeData.vue
+++ src/components/employeeData.vue
@@ -1,82 +1,88 @@
 <template>
-
-<div class="horizontal">
-<div class="block" v-for="(data, index) in AllData" :key="index">
-    <div>First name : {{data.first_name}}</div>
-    <div>Last name : {{data.last_name}}</div>
-    <div>ID number : {{data.ID_number}}</div>
-    <div>Department : {{data.Department}}</div>
-    <button @click="deleteData(data, data._id)">Delete</button>
-</div>
-</div>
+  <div class="horizontal">
+    <div class="block" v-for="(data, index) in AllData" :key="index">
+      <div>First name : {{ data.first_name }}</div>
+      <div>Last name : {{ data.last_name }}</div>
+      <div>ID number : {{ data.ID_number }}</div>
+      <div>Department : {{ data.Department }}</div>
+      <button @click="deleteData(data, data._id)">Delete</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import { registerRuntimeCompiler } from "vue";
 
 export default {
-    name: 'employeeData',
-    
-    data() {
-        return {
-            msg: "Good",
-            reciveData: [],
-            limit: 8,
-        }
-    },
-    methods: {
-    deleteData(reciveData, _id) {
-        axios.delete('http://localhost:3000/delete-employee/' + _id)
-        .then(response => {
-          return console.log(response)
-        })
-        .catch(error => console.log(error)); 
-        
+  name: "employeeData",
 
-        //window.location.reload();
-
-    }
-    },
-    created() {
-        axios.get('http://localhost:3000/show-employee')
-        .then((response) => {
-            this.reciveData = response.data;
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    },
-    computed: {
-        AllData() {
-            return this.reciveData.slice(0, this.limit);
-            }
-        },
+  data() {
+    return {
+      msg: "Good",
+      reciveData: [],
+      limit: 8,
     };
+  },
+  methods: {
+    deleteData(reciveData, _id) {
+      axios
+        .delete("http://localhost:3000/delete-employee/" + _id)
+        .then((response) => {
+          return console.log(response);
+        })
+        .catch((error) => console.log(error));
+
+      //window.location.reload();
+    },
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/show-employee")
+      .then((response) => {
+        this.reciveData = response.data;
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  computed: {
+    AllData() {
+      console.log(this.reciveData);
+      return this.reciveData.slice(0, this.limit);
+    },
+  },
+  updated() {
+      if (this.reciveData.length === 8) {
+        this.$emit("EmitDataLimit", true)
+      }
+    },
+  };
 </script>
 
 <style>
 .delete {
-    position: absolute;
-    top: 0.1;
-    right: 0;
-    border-radius: 50%;
-    padding: 8px;
+  position: absolute;
+  top: 0.1;
+  right: 0;
+  border-radius: 50%;
+  padding: 8px;
 }
 .delete:hover {
-    cursor: pointer;
-    box-shadow: 1px 2px 3px rgba(0, 0, 0, 2);
+  cursor: pointer;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 2);
 }
 .block {
-    width: 200px;
-    border-radius: 10px;
-    background: rgb(190, 143, 71);
-    color: white;
-    text-align: center;
-    padding: 50px 0;
-    margin:  30px 10px auto;
+  width: 200px;
+  border-radius: 10px;
+  background: rgb(190, 143, 71);
+  color: white;
+  text-align: center;
+  padding: 50px 0;
+  margin: 30px 10px auto;
 }
 .horizontal {
-    display: flex;
+  display: flex;
 }
 </style>
