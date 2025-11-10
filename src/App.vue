1343b07fa2a401e6e9a4f76d8ab296895c7f8bc5
Tomek <sigma345@wp.pl>
2025-11-10 17:49:56 +0100

    remove test value

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <h1>Sorting Management App</h1>
 
-<div v-for="item in items" :key="item.id">
-  <v-tooltip bottom>
-    <template v-slot:activator="{ on, attrs }">
-        <span v-bind="attrs" v-on="on">Hover to View</span>
-    </template>
-    <span>{{ item.name }}</span>
-  </v-tooltip>
-</div>
-
-<button v-tooltip="coś"/>
-       
-<div>
-<v-tooltip v-for="test in tests2" v-bind:key="test">
-
-<button  > UseMe or no</button>
-<p>{{title}}</p>
-</v-tooltip>
- </div>
-<v-btn>
-  botton
-</v-btn>
-
 
   <div v-if="showForm">
     <AddEmployeeData @closeFormEmit="ActiveForm" :limit="limit"/>
@@ -37,25 +15,15 @@
 import AddEmployeeData from "./components/AddEmployeeData.vue";
 import employeeData from "./components/employeeData.vue";
 
-
-
-
 export default {
   name: "App",
-  components: { AddEmployeeData, employeeData },
+  components: { AddEmployeeData, employeeData},
 
   data() {
     return {
       title: "Sorting Management hub",
       showForm: false,
       limit: false,
-      tests2: ["baran", "kozak", "goofy", "maslo"],
-      tests: [{id1:"test1"}, {id:"test2"}, {id:"test3"}, {id:"test4"}, {id:"test5"}],
-      show: true,
-       items: [
-        { id: 1, name: 'Item 1', description: 'This is item 1.' },
-        { id: 2, name: 'Item 2', description: 'This is item 2.' },
-      ],
     };
   },
   methods: {
