aa5086be324f5cf371f56a9dd802a4085a4f86cc
Tomek <sigma345@wp.pl>
2025-11-10 15:26:05 +0100

    try to add tooltip with button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
   <h1>Sorting Management App</h1>
-<button v-tooltip="'You have new messages.'" > UseMe or no</button>
+
+<div v-for="item in items" :key="item.id">
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+        <span v-bind="attrs" v-on="on">Hover to View</span>
+    </template>
+    <span>{{ item.name }}</span>
+  </v-tooltip>
+</div>
+
+<button v-tooltip="coś"/>
+       
+<div>
+<v-tooltip v-for="test in tests2" v-bind:key="test">
+
+<button  > UseMe or no</button>
+<p>{{title}}</p>
+</v-tooltip>
+ </div>
+<v-btn>
+  botton
+</v-btn>
+
 
   <div v-if="showForm">
     <AddEmployeeData @closeFormEmit="ActiveForm" :limit="limit"/>
@@ -27,6 +49,13 @@ export default {
       title: "Sorting Management hub",
       showForm: false,
       limit: false,
+      tests2: ["baran", "kozak", "goofy", "maslo"],
+      tests: [{id1:"test1"}, {id:"test2"}, {id:"test3"}, {id:"test4"}, {id:"test5"}],
+      show: true,
+       items: [
+        { id: 1, name: 'Item 1', description: 'This is item 1.' },
+        { id: 2, name: 'Item 2', description: 'This is item 2.' },
+      ],
     };
   },
   methods: {
