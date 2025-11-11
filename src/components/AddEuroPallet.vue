51f820d86477aac874fc001b7af8a5c068e5b9da
Tomek <sigma345@wp.pl>
2025-11-11 18:44:24 +0100

    create addEuropalet and add data and addToApi axio

---
+++ src/components/AddEuroPallet.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="backdrop">
+<form>
+    <label> ID Worker</label>
+    <input v-model="EuroPallet.ID_Worker" />
+</form>
+
+</div>
+</template>
+
+<script>
+
+export default {
+    name: "AddEuroPallet",
+
+    data() {
+        return{
+        EuroPallet: {
+        ID_Worker: "",
+        keyPallet: 0,
+        quantity: 0,
+        nestet: "",
+        department: "",
+        location: "",
+        codeForTickets: 0,
+        codeForSelectors: 0,
+        },
+    };
+},
+    methods: {
+        addToAPI() {
+        let newEuroPallet = {
+            ID_Worker: this.ID_Worker,
+            keyPallet: this.keyPallet,
+            quantity: this.quantity,
+            nestet: this.nestet,
+            department: this.department,
+            location: this.location,
+            codeForTickets: this.codeForTickets,
+            codeForSelektors: this.codeForSelectors,
+        };
+        axios
+        .post("http://localhost:3000/create-EuroPallet", newEuroPallet)
+        .then((response) =>{
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        },
+    }
+}
+</script>
+
+<style>
+</style>
+
