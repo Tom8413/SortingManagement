6beedd25c028c23de1b237ea62d070901ee81600
Tomek <sigma345@wp.pl>
2025-11-11 21:53:08 +0100

    remove "," from shemaPallet

--- modules/ShemeEuroPallet.js
+++ modules/ShemeEuroPallet.js
@@ -1,40 +1,40 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const euroPalletSchema = new Schema ({
+const euroPalletSchema = new Schema({
     ID_Worker: {
         type: String,
-        Required: 'Please enter',
+        Required: 'Please enter'
     },
     keyPallet: {
         type: Number,
-        Required: 'Please enter',
+        Required: 'Please enter'
     },
     quantity: {
         type: Number,
-        Reqired: 'Please enter',
+        Reqired: 'Please enter'
     },
     nested: {
         type: String,
-        Required: 'Please enter',
+        Required: 'Please enter'
     },
     department: {
         type: String,
-        Required: 'Please enter',
+        Required: 'Please enter'
     },
     location: {
         type: String,
-        Reuired: 'Please enter',
+        Reuired: 'Please enter'
     },
     codeForTickets: {
         type: Number,
-        Required: 'Please enter',
+        Required: 'Please enter'
     },
     codeForSelectors: {
         type: Number,
-        Required: 'Please enter',
-    },
+        Required: 'Please enter'
+    }
 }, {timestamps: true});
 
 const EuroPallets = mongoose.model('EuroPallet', euroPalletSchema);
-module.export = EuroPallets;
+module.exports = EuroPallets;
