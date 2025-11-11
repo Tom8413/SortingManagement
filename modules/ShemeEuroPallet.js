11832da1bcd5f03c7e63a8de69ce1681bb09169e
Tomek <sigma345@wp.pl>
2025-11-11 18:38:01 +0100

    add ShemeEuroPallet

---
+++ modules/ShemeEuroPallet.js
@@ -0,0 +1,40 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+const euroPalletSchema = new Schema ({
+    ID_Worker: {
+        type: String,
+        Required: 'Please enter',
+    },
+    keyPallet: {
+        type: Number,
+        Required: 'Please enter',
+    },
+    quantity: {
+        type: Number,
+        Reqired: 'Please enter',
+    },
+    nested: {
+        type: String,
+        Required: 'Please enter',
+    },
+    department: {
+        type: String,
+        Required: 'Please enter',
+    },
+    location: {
+        type: String,
+        Reuired: 'Please enter',
+    },
+    codeForTickets: {
+        type: Number,
+        Required: 'Please enter',
+    },
+    codeForSelectors: {
+        type: Number,
+        Required: 'Please enter',
+    },
+}, {timestamps: true});
+
+const EuroPallets = mongoose.model('EuroPallet', euroPalletSchema);
+module.export = EuroPallets;
