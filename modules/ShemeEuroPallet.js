fcdd6456405c10f9e923c72ef2bb61c035887c85
Tomek <sigma345@wp.pl>
2025-11-12 20:11:32 +0100

    remove data from shema

--- modules/ShemeEuroPallet.js
+++ modules/ShemeEuroPallet.js
@@ -4,36 +4,8 @@ const Schema = mongoose.Schema;
 const euroPalletSchema = new Schema({
     ID_Worker: {
         type: String,
-        Required: 'Please enter'
+        Required: true
     },
-    keyPallet: {
-        type: Number,
-        Required: 'Please enter'
-    },
-    quantity: {
-        type: Number,
-        Reqired: 'Please enter'
-    },
-    nested: {
-        type: String,
-        Required: 'Please enter'
-    },
-    department: {
-        type: String,
-        Required: 'Please enter'
-    },
-    location: {
-        type: String,
-        Reuired: 'Please enter'
-    },
-    codeForTickets: {
-        type: Number,
-        Required: 'Please enter'
-    },
-    codeForSelectors: {
-        type: Number,
-        Required: 'Please enter'
-    }
 }, {timestamps: true});
 
 const EuroPallets = mongoose.model('EuroPallet', euroPalletSchema);
