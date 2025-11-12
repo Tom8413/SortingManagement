24158b067a11227ec4af7eeaa45132f5ab875da9
Tomek <sigma345@wp.pl>
2025-11-12 20:13:41 +0100

    remove data form addeuroPallet

--- src/components/AddEuroPallet.vue
+++ src/components/AddEuroPallet.vue
@@ -6,27 +6,6 @@
              required 
              maxlength="15" 
              v-model="EuroPallet.ID_Worker" />
-
-      <label>Last Name:</label>
-      <input type="text"
-             maxlength="15"  
-             required 
-             v-model="Employee.last_name" />
-
-      <label>ID_number:</label>
-      <input type="text"
-             onkeypress='return event.charCode >= 48 && event.charCode <= 57'
-             maxlength="8" 
-             required
-             v-model="Employee.ID_number" />
-
-      <label>Department: </label>
-      <select v-model="Employee.Department">
-        <option value="NCP">NCP</option>
-        <option value="NCC">NCC</option>
-        <option value="STM">STM</option>
-        <option value="Kids">Kids</option>
-      </select>
     
       <button type="button" 
               :disabled="MeetConditions" 
@@ -39,6 +18,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "AddEuroPallet",
 
@@ -46,30 +27,18 @@ export default {
     return {
       EuroPallet: {
         ID_Worker: "",
-        keyPallet: 0,
-        quantity: 0,
-        nestet: "",
-        department: "",
-        location: "",
-        codeForTickets: 0,
-        codeForSelectors: 0,
+    
       },
     };
   },
   methods: {
     addToAPI() {
       let newEuroPallet = {
-        ID_Worker: this.ID_Worker,
-        keyPallet: this.keyPallet,
-        quantity: this.quantity,
-        nestet: this.nestet,
-        department: this.department,
-        location: this.location,
-        codeForTickets: this.codeForTickets,
-        codeForSelektors: this.codeForSelectors,
+        ID_Worker: this.EuroPallet.ID_Worker,
+
       };
       axios
-        .post("http://localhost:3000/create-EuroPallet", newEuroPallet)
+        .post("http://localhost:3000/", newEuroPallet)
         .then((response) => {
           console.log(response);
         })
@@ -81,6 +50,14 @@ export default {
       this.$emit("closeFormEmitPallet");
     },
   },
+  computed: {
+    MeetConditions() {
+      if (this.EuroPallet.ID_Worker === "" ) 
+          
+        return true 
+      
+      },
+    },
 };
 </script>
 
