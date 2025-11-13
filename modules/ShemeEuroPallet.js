c82c25cc55f14c85729fc2431803cd3a1ac65f90
Tomek <sigma345@wp.pl>
2025-11-13 21:58:07 +0100

    add data to shemaEuroPallet

--- modules/ShemeEuroPallet.js
+++ modules/ShemeEuroPallet.js
@@ -10,6 +10,22 @@ const euroPalletSchema = new Schema({
         type: Number,
         Required: 'Please enter'
     },
+    Quantity: {
+        type: Number,
+        Required: 'Please enter'
+    },
+    Nestet: {
+        type: String,
+        Required: 'Please enter'
+    },
+    Depertment: {
+        type: String,
+        Required: 'Please enter'
+    },
+    Location: {
+        type: String,
+        Required: 'Please enter'
+    },
 }, {timestamps: true});
 
 const EuroPallets = mongoose.model('EuroPallet', euroPalletSchema);
