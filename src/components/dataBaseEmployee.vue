a031498c82bd5b00749382b73905157ed01999f5
Tomek <sigma345@wp.pl>
2025-11-14 21:55:12 +0100

    add dataBase Employee vue

---
+++ src/components/dataBaseEmployee.vue
@@ -0,0 +1,21 @@
+<template>
+<h1> 
+    Data Base information
+</h1>
+</template>
+
+<script>
+
+export default {
+    name: "dataBaseEmployee",
+
+    data() {
+        return{
+
+        };
+    },
+}
+</script>
+
+<style>
+</style>
