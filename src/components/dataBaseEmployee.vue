bd05884b428dcc63e570fe243ff56fa2fc195f6c
Tomek <sigma345@wp.pl>
2025-11-20 21:23:40 +0100

    add emit data

--- src/components/dataBaseEmployee.vue
+++ src/components/dataBaseEmployee.vue
@@ -8,13 +8,12 @@
     <button type="button" @click="ActiveFormEmployee">Add Employee</button>
   </label>
   <div>
-    <EmployeeList />
+    <EmployeeList @EmitDataEmployee="ReciveDataEmployee" /> <!--recive data from child - EmployeeList--> 
   </div>
   
 </template>
 
 <script>
-import axios from "axios";
 import AddEmployeeData2 from "./AddEmployeeData2.vue";
 import EmployeeList from "./EmployeeList.vue";
 
@@ -24,16 +23,21 @@ export default {
 
   data() {
     return {
-
       showFormEmployee: false,
+      dataEmployee: [],
     };
   },
   methods: {
-  
     ActiveFormEmployee() {
       this.showFormEmployee = !this.showFormEmployee;
     },
-}
+    ReciveDataEmployee: function(params) {
+      this.dataEmployee = params;
+    }
+},
+  updated() {
+        this.$emit("EmitDataPTA", this.dataEmployee) //emit data from parent to Ancestor
+      }
 }
 </script>
 
